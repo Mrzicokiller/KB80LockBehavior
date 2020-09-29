1e356b8f12ddc163921488e95c5350baf062261b
Zico Moerland <zico_moerland@live.nl>
2020-09-29 14:25:42 +0200

    Add basic observer for lockEvent

--- LockConsole/Program.cs
+++ LockConsole/Program.cs
@@ -1,12 +1,41 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 
 namespace LockConsole
 {
     class Program
     {
+        static bool isLocked { get; set; } = false;
+        static bool isInActive { get; set; } = false;
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            SystemEvents.SessionSwitch += new SessionSwitchEventHandler(SystemEvents_SessionSwitch);
+            Console.ReadLine();
         }
+
+        // Observer: When a SystemEvent.SessionSwitch is fired checks if SessionLock event is fired
+        static void SystemEvents_SessionSwitch(object sender, SessionSwitchEventArgs e)
+        {
+            if (e.Reason == SessionSwitchReason.SessionLock)
+            {
+                Console.WriteLine("locked");
+                isLocked = true;
+            }
+            else if (e.Reason == SessionSwitchReason.SessionUnlock)
+            {
+                Console.WriteLine("unlocked");
+                isLocked = false;
+            }
+        }
+
     }
+    /*
+     Test code:
+     for(int i = 0; i < 10; i++)
+     {
+        Console.WriteLine("Boolean isLocked: " + isLocked);
+        Thread.Sleep(500);
+     }
+     */
 }
